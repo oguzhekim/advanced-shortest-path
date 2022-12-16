0edfceaff5b0541e566f21b2813761780f4ee063
Oguz <mustafaoguzhekim@gmail.com>
2022-12-16 20:10:58 +0300

    Reading can still be improved.

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,40 +1,52 @@
 import java.util.*;
 
 public class Simulator {
+    // Entry that will enter PQ of Dijkstra.
     private record QueueEntry(Vertex vertex, int distance) {
     }
     public int dijkstra(Vertex start, Vertex end, int v) {
         PriorityQueue<QueueEntry> queue = new PriorityQueue<>(Comparator.comparingInt(QueueEntry::distance));
+        // Every vertex has an index field that points them to the arrays below.
+        // At the beginning all the nodes are unvisited.
         boolean[] visited = new boolean[v];
+        // Distances set to infinity at the beginning.
         int[] distance = new int[v];
         Arrays.fill(distance, Integer.MAX_VALUE);
+        // Enqueue start vertex and set distance to 0.
         distance[start.index] = 0;
         queue.add(new QueueEntry(start, 0));
         while (!queue.isEmpty()){
             Vertex v1 = queue.poll().vertex;
+            // I'm not doing queue.remove() operations, so there might be multiple entries of same vertex in the queue.
+            // So I'm checking if that node is visited to improve performance.
             if (visited[v1.index])
                 continue;
             visited[v1.index] = true;
+            // Iterate edges of current vertex.
             for (Edge e: v1.getAdjList()){
                 Vertex v2 = e.destination();
+                // This block further increases performance by skipping that edge if the destination is already visited.
                 if (visited[v2.index])
                     continue;
+                // We will enqueue a new entry if a new shortest path is found.
                 int val = distance[v1.index]+e.weight();
                 if (distance[v2.index] > val){
                     distance[v2.index] = val;
                     queue.add(new QueueEntry(v2, val));
                 }
             }
+            // We don't need to find the shortest path to all vertices. Once end vertex is visited we can stop Dijkstra to improve performance.
             if (v1.equals(end))
                 return distance[v1.index];
         }
+        // If queue becomes empty without visiting end vertex, that means end vertex is not connected to the start vertex.
         return -1;
     }
     public int flag(ArrayList<Vertex> flags, int flagCount, int v){
         int[][] matrix = new int[flagCount][flagCount];
         for (int[] row: matrix)
             Arrays.fill(row, Integer.MAX_VALUE-1);
-
+        // Run Dijkstra for each flag to other flags and store the distances to matrix above.
         for (int i=0; i<flagCount; i++) {
             int visitedFlags = 0;
             Vertex start = flags.get(i); //TODO: Make flags array
@@ -66,20 +78,43 @@ public class Simulator {
                 }
             }
         }
-
-        HashSet<Integer> visitedColumns = new HashSet<>();
-        HashSet<Integer> visitedRows = new HashSet<>();
-        visitedRows.add(0);
-        visitedColumns.add(0);
+        // Shortest distance from a flag to any other flag is determined and stored in matrix.
+        HashSet<Integer> rowsToVisit = new HashSet<>();
+        rowsToVisit.add(0);
         int count = 0;
-        // Main idea of this matrix is visitedRows are the only rows that can be visited (and should be visited). And visitedColumns cannot be visited again.
-        while (visitedColumns.size()!=flagCount){
+        // Main idea of this matrix is rowsToVisit are the only rows that can be visited (and should be visited). And visitedColumns cannot be visited again.
+        // So start with row 0, and we can't visit column 0 as well because we don't want to consider distance of a flag to itself.
+        // For inp8 initial matrix is:
+        //          s3 s6  s7
+        //     s3   |0  3  6|
+        //     s6   |3  0  4|
+        //     s7   |6  4  0|
+        // We start with s3 and get the minimum cell. But we do not consider column containing s3 because we are already visiting it right now.
+        //          s3 s6  s7
+        //     s3   |x  3  6|
+        //     s6   |x  0  4|
+        //     s7   |x  4  0|
+        // Minimum is 3 on the step above, it's added to count.
+        // Since minimum corresponds to s6, now we will visit both s3 and s6.
+        // But we won't consider columns of s3 and s6 again because these vertices are visited and added to count.
+        //          s3 s6  s7
+        //     s3   |x  x  6|
+        //     s6   |x  x  4|
+        //     s7   |x  x  0|
+        // Minimum is 4 on the step above, it's added to count.
+        // Since minimum corresponds to s7, now we will visit both s3, s6 and s7.
+        // But we won't consider columns of s3, s6 and s7 again because these vertices are visited and added to count.
+        //          s3 s6  s7
+        //     s3   |x  x  x|
+        //     s6   |x  x  x|
+        //     s7   |x  x  x|
+        // Since there no cell left to visit code will terminate and return count as 7.
+        while (rowsToVisit.size()!=flagCount){
             int min = Integer.MAX_VALUE;
             int rowToAdd = 0;
-            int colToAdd = 0;
-            for (int row: visitedRows){
+            for (int row: rowsToVisit){
                 for (int col=0; col<flagCount; col++){
-                    if (!visitedColumns.contains(col)){
+                    if (!rowsToVisit.contains(col)){
                         int cell = matrix[row][col];
                         // If the cell contains this number, it means flags are not connected.
                         if (cell == Integer.MAX_VALUE-1)
@@ -88,13 +123,11 @@ public class Simulator {
                         if (min>cell){
                             min = cell;
                             rowToAdd = col;
-                            colToAdd = col;
                         }
                     }
                 }
             }
-            visitedRows.add(rowToAdd);
-            visitedColumns.add(colToAdd);
+            rowsToVisit.add(rowToAdd);
             count+=min;
         }
         return count;
--- src/project4.java
+++ src/project4.java
@@ -1,64 +1,77 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class project4 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        int index = 0;
+        FileReader fr = new FileReader("smallCases/input/inp13.txt");
+        BufferedReader br = new BufferedReader(fr);
         Graph graph = new Graph(); //TODO: Get rid of vertices map and remove graph class
         HashMap<String, Vertex> vertices = graph.getVertices();
         ArrayList<Vertex> flags = new ArrayList<>();//TODO: Make this hashset
-        File input = new File("largeCases/input/stressPart1-2.txt");
-        Scanner reader = new Scanner(input);
-        int vertexCount = Integer.parseInt(reader.nextLine());
-        int flagCount = Integer.parseInt(reader.nextLine());
-        String[] thirdLine = reader.nextLine().split(" ");
-        Vertex startVertex = new Vertex(thirdLine[0], index++);
-        Vertex finishVertex = new Vertex(thirdLine[1], index++);
-        vertices.put(thirdLine[0], startVertex);
-        vertices.put(thirdLine[1], finishVertex);
-        String[] fourthLine = reader.nextLine().split(" ");
-        // Create vertices with flags
-        for (String s : fourthLine) {
+        int index = 0;
+        int vertexCount = Integer.parseInt(br.readLine()); // First line
+        int flagCount = Integer.parseInt(br.readLine()); // Second line
+        String thirdLine = br.readLine();
+        StringTokenizer tk = new StringTokenizer(thirdLine);
+        Vertex startVertex = new Vertex(tk.nextToken(), index++);
+        vertices.put(startVertex.getName(), startVertex);
+        Vertex end = new Vertex(tk.nextToken(), index++);
+        vertices.put(end.getName(), end);
+        // FLAGS
+        tk = new StringTokenizer(br.readLine());
+        String s = tk.nextToken(); // First flag
+        while (s!=null){
             if (startVertex.getName().equals(s)) {
                 startVertex.setFlag(true);
-                flags.add(startVertex);//
-            } else if (finishVertex.getName().equals(s)) {
-                finishVertex.setFlag(true);
-                flags.add(finishVertex);//
+                flags.add(startVertex);
+            } else if (end.getName().equals(s)) {
+                end.setFlag(true);
+                flags.add(end);
             } else {
                 Vertex v = new Vertex(s, index++);
                 v.setFlag(true);
                 vertices.put(s, v);
                 flags.add(v);
             }
+            try{
+                s = tk.nextToken();
+            } catch (NoSuchElementException e){
+                s = null;
+            }
         }
-        for (int i=0; i<vertexCount; i++){
-            String[] line = reader.nextLine().split(" ");
-            Vertex v1 = vertices.get(line[0]);
-            // If vertex not in vertexList.
+        // EDGES
+        String currentLine =br.readLine();
+        while (currentLine != null) {
+            tk = new StringTokenizer(currentLine);
+            s = tk.nextToken();
+            Vertex v1 = vertices.get(s);
             if (v1 == null){
-                v1 = new Vertex(line[0], index++);
-                vertices.put(line[0], v1);
+                v1 = new Vertex(s, index++);
+                vertices.put(s, v1);
             }
-            for (int j=1; j<line.length; j+=2){
-                Vertex v2;
-                if (!vertices.containsKey(line[j])){
-                    v2 = new Vertex(line[j], index++);
-                    vertices.put(line[j], v2);
+            while (s!=null){
+                try{
+                    s = tk.nextToken();
+                    Vertex v2 = vertices.get(s);
+                    if (v2 == null){
+                        v2 = new Vertex(s, index++);
+                        vertices.put(s, v2);
+                    }
+                    s = tk.nextToken();
+                    v1.adjAdd(new Edge(v2, Integer.parseInt(s)));
+                    v2.adjAdd(new Edge(v1, Integer.parseInt(s)));
+                } catch (NoSuchElementException e){
+                    s = null;
                 }
-                else
-                    v2 = vertices.get(line[j]);
-                v1.adjAdd(new Edge(v2, Integer.parseInt(line[j+1])));
-                v2.adjAdd(new Edge(v1, Integer.parseInt(line[j+1])));
             }
+            currentLine = br.readLine();
         }
         long cp1 = System.currentTimeMillis();
         System.out.println("reading:" + (cp1 - start));
-        Simulator s = new Simulator();
-        System.out.println(s.dijkstra(startVertex,finishVertex, vertexCount));
-        System.out.println(s.flag(flags, flagCount, vertexCount));
+        Simulator sim = new Simulator();
+        System.out.println(sim.dijkstra(startVertex,end, vertexCount));
+        System.out.println(sim.flag(flags, flagCount, vertexCount));
         long finish = System.currentTimeMillis();
         System.out.println("total:" + (finish - start));
     }
